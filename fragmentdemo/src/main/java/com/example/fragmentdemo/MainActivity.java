3246f7b17819d2f4ec3388e99c6998239d60dc28
jiangxiaolin@J1002280 <2524811317@qq.com>
2019-07-23 19:42:26 +0800

    增加了一个调试信息

--- fragmentdemo/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ fragmentdemo/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fragmentdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         Log.v("TAG","onCreate");
+
         Myfragment1 name=new Myfragment1();
         getFragmentManager().beginTransaction().replace(R.id.LinearLayout1,name).commit();
     }
